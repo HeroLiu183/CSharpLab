da2e56637d9f4410d9fcfb296e264043be55fd49
heroliu183 <heroliu183@gmail.com>
2017-06-20 13:39:56 +0800

    inital solution
    lib project and testing project
    and add one test case first

---
+++ CSharpLab/ExtensionMethod/ParsStringToNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpLab.ExtensionMethod
+{
+    public static class ParsStringToNumber
+    {
+        public static int ParsToInt(this string intString )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void PrintType(this MyClass myClass)
+        {
+            Console.WriteLine(myClass.Type)S;
+        }
+    }
+}
---
+++ CSharpLabTests/ExtensionMethodTests/ExtensionMethodTest.cs
@@ -0,0 +1,71 @@
+﻿using CSharpLab.ExtensionMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpLabTests.ExtensionMethodTests
+{
+    /// <summary>
+    /// Summary description for ExtensionMethodTest
+    /// </summary>
+    [TestClass]
+    public class ExtensionMethodTest
+    {
+        public ExtensionMethodTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void String_Pars_To_Int_Extension_Method_Tests()
+        {
+            //Arrange
+            var target = "100";
+            var expect = 100;
+            //Act
+            var actual = target.ParsToInt();
+            //Assert
+            Assert.AreEqual(expect,actual);
+        }
+    }
+}
